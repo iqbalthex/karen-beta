a1d567db4d1fae95a46fc3f9f41b838f8508fe70
{{ iqbal.a.m }} <73935864+iqbalthex@users.noreply.github.com>
2023-06-18 06:55:30 +0700

    Delete src directory

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-import { getAll, getLatest, updateAll, updateLatest } from "./handlers.js";
-
-export default [
-  { method: "GET",  path: "/all",    handler: getAll },
-  { method: "GET",  path: "/latest", handler: getLatest },
-  { method: "POST", path: "/all",    handler: updateAll },
-  { method: "POST", path: "/latest", handler: updateLatest },
-];
